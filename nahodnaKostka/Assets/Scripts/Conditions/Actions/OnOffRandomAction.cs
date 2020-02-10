4405e701491b76b988dd7a872dbe7713e0ca521c
OFaltys <OndrejFaltys@seznam.cz>
2020-02-10 09:27:15 +0100

    .

---
+++ nahodnaKostka/Assets/Scripts/Conditions/Actions/OnOffRandomAction.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[AddComponentMenu("Playground/Actions/On-Off")]
+public class OnOffRandomAction : Action
+{
+
+	public int max=6, min=1, vyhrava=6;
+	public float jakDlouho=3;
+	public GameObject SkupinaObsahujiciMoznosti;
+	public GameObject odhalVyhru;
+	public GameObject odhalProhu;
+
+	private bool hazise = false;
+	private int rozsah=6, predtim = 1;
+
+	// Changes the object state from active to inactive, and viceversa
+	public override bool ExecuteAction(GameObject dataObject)
+	{
+		rozsah = max - min + 1;
+		hazise = true;
+		jakDlouho += Random.Range( min, max*10) / 60;
+		StartCoroutine(hodkostky());
+
+		return true;
+	}
+
+	void Update(){
+		if(hazise){
+			if(predtim < max-1){
+				SkupinaObsahujiciMoznosti.transform.GetChild(predtim).gameObject.SetActive(false);
+				predtim++;
+				SkupinaObsahujiciMoznosti.transform.GetChild(predtim).gameObject.SetActive(true);
+			}
+			else{
+				SkupinaObsahujiciMoznosti.transform.GetChild(predtim).gameObject.SetActive(false);
+				predtim = 0;
+				SkupinaObsahujiciMoznosti.transform.GetChild(predtim).gameObject.SetActive(true);
+			}
+		}
+	}
+
+	IEnumerator hodkostky()
+	{
+		yield return new WaitForSeconds( jakDlouho );
+		hazise = false;
+		if(predtim + 1 == vyhrava)
+			odhalVyhru.gameObject.SetActive(true);
+		else
+			odhalProhu.gameObject.SetActive(true);
+		this.gameObject.SetActive(false);
+	}
+}
---
+++ novy/Assets/neco.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class neco : MonoBehaviour
+{
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        int reference = new int(8);
+		reference = 8; 
+		int hodnota = 4;
+
+		int A = reference;
+		int B = hodnota;
+
+		print("1)"+reference +" "+ hodnota+"/"+A+" "+B);
+
+		reference++; 
+		hodnota++;
+
+		print("2)"+reference +" "+ hodnota+"/"+A+" "+B);
+
+		A++;
+		B++;
+
+		print("3)"+reference +" "+ hodnota+"/"+A+" "+B);
+		
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
